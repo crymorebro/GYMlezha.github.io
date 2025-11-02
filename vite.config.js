c1c5d669e9373167d12ed3695bbeeeaf711fd24c
crymorebro <kolakudasev1840@gmail.com>
2025-11-02 18:48:41 +0300

    deploy: build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/crymorebro.github.io/",
+  base:"/GYMlezha.github.io/",
   plugins: [
     vue(),
     vueDevTools(),
